2bfee86d4325421e46d16f30807a2bf43fc77a22
BambooOnFire <64479852+BambooOnFire@users.noreply.github.com>
2021-09-22 11:36:36 +0900

    Update RUN.py

--- RUN.py
+++ RUN.py
@@ -79,19 +79,23 @@ ydl_opts = {
     'progress_hooks': [my_hook]
 }
 
-Links = input("Enter youtube URLs, separated with a comma and a space, that you want to download and render. If not, just type any random stuff and press ENTER: ")
-Links = Links.split(", ")
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    for x in Links:
-        try:
-            ydl.download([x])           # If Certificate Error pops up on OSX, https://stackoverflow.com/questions/42098126/mac-osx-python-ssl-sslerror-ssl-certificate-verify-failed-certificate-verify
-        except:
-            pass
+yes = ['y', 'Y', 'yes', 'Yes', 'YES']
+
+youtube = input("Do you want to also render with YouTube URLs?: ")
+if any(x in youtube for x in yes):
+    Links = input("Enter youtube URLs, separated with a comma and a space, that you want to download and render: ")
+    Links = Links.split(", ")
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        for x in Links:
+            try:
+                ydl.download([x])           # If Certificate Error pops up on OSX, https://stackoverflow.com/questions/42098126/mac-osx-python-ssl-sslerror-ssl-certificate-verify-failed-certificate-verify
+            except:
+                pass
 
 for path in os.listdir(Input):
     if not path.startswith("."):        # IGNORE .DS_STORE or any system files
         full_path = os.path.join(Input,path)
-        print("\n" + str(full_path) + "\n")
+        print("\n" + 'RENDERING: ' + str(full_path) + "\n")
         
         # Convert to mp3 audio type
         if not full_path.endswith('.mp3'):
@@ -101,10 +105,10 @@ for path in os.listdir(Input):
             try:
                 raw_audio.export(export_path, format="mp3")
                 os.remove(full_path)
-                print("Conversion Successful")
+                print("CONVERSION Successful")
                 full_path = export_path
             except:
-                print("Conversion Error")
+                print("CONVERSION Error")
        
         # Load audio
         (audio, _) = load_audio(full_path, sr=sample_rate, mono=True)
